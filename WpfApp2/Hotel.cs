34af7b092ce1fede2b5b8a0c769925d8e92e7860
Nikita <Nikita@DESKTOP-9UT07R5>
2025-04-10 13:18:07 +0300

    fgf

--- WpfApp2/Hotel.cs
+++ WpfApp2/Hotel.cs
@@ -11,6 +11,7 @@ namespace WpfApp2
         public int Id { get; set; }
 
 
+
         public string Name { get; set; }
         public int CountOfStars { get; set; }
         public string CountryName { get; set; }
